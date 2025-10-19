7e292e79ab6e85e86fe5695e78432f919415fef0
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-10-19 13:43:15 -0700

    Move createTransformerWithState into its own file to be shared
    FileRenderer will require such things

--- packages/precision-diffs/src/DiffHunksRenderer.ts
+++ packages/precision-diffs/src/DiffHunksRenderer.ts
@@ -28,16 +28,13 @@ import type {
   ThemeTypes,
   ThemesRendererOptions,
 } from './types';
+import { createTransformerWithState } from './utils/createTransformerWithState';
 import { formatCSSVariablePrefix } from './utils/formatCSSVariablePrefix';
 import { getFiletypeFromFileName } from './utils/getFiletypeFromFileName';
 import {
-  convertLine,
-  createAnnotationElement,
-  createEmptyRowBuffer,
   createHastElement,
   createPreWrapperProperties,
   createSeparator,
-  findCodeElement,
 } from './utils/hast_utils';
 import { parseLineType } from './utils/parseLineType';
 
@@ -1086,65 +1083,6 @@ function createDiffSpanDecoration({
     end: { line, character: spanStart + spanLength },
     properties: { 'data-diff-span': '' },
     alwaysWrap: true,
-  };
-}
-
-function createTransformerWithState(disableLineNumbers: boolean): {
-  state: SharedRenderState;
-  transformer: ShikiTransformer;
-} {
-  const state: SharedRenderState = {
-    lineInfo: {},
-    decorations: [],
-    disableLineNumbers,
-  };
-  return {
-    state,
-    transformer: {
-      line(hast) {
-        // Remove the default class
-        delete hast.properties.class;
-        return hast;
-      },
-      pre(pre) {
-        // NOTE(amadeus): This kinda sucks -- basically we can't apply our
-        // line node changes until AFTER decorations have been applied
-        const code = findCodeElement(pre);
-        const children: ElementContent[] = [];
-        if (code != null) {
-          let index = 1;
-          for (const node of code.children) {
-            if (node.type !== 'element') {
-              continue;
-            }
-            // Do we need to inject an empty span above the first line line?
-            if (index === 1 && state.lineInfo[0]?.spans != null) {
-              for (const span of state.lineInfo[0]?.spans ?? []) {
-                if (span.type === 'gap') {
-                  children.push(createEmptyRowBuffer(span.rows));
-                } else {
-                  children.push(createAnnotationElement(span));
-                }
-              }
-            }
-            children.push(convertLine(node, index, state));
-            const lineInfo = state.lineInfo[index];
-            if (lineInfo?.spans != null) {
-              for (const span of lineInfo.spans) {
-                if (span.type === 'gap') {
-                  children.push(createEmptyRowBuffer(span.rows));
-                } else {
-                  children.push(createAnnotationElement(span));
-                }
-              }
-            }
-            index++;
-          }
-          code.children = children;
-        }
-        return pre;
-      },
-    },
   };
 }
 
