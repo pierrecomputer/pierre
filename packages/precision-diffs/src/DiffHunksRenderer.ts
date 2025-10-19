246dd95a80d1e1b018ea8aa35d164562bc85d9b5
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-10-19 14:51:24 -0700

    DiffHunksRenderer: don't modify preNode when rendering

--- packages/precision-diffs/src/DiffHunksRenderer.ts
+++ packages/precision-diffs/src/DiffHunksRenderer.ts
@@ -366,8 +366,10 @@ export class DiffHunksRenderer<LAnnotation = undefined> {
   }
 
   renderFullHTML(result: HunksRenderResult): string {
+    const _children = result.preNode.children;
+    const tempChildren = [..._children];
     if (result.unifiedAST != null) {
-      result.preNode.children.push(
+      tempChildren.push(
         createHastElement({
           tagName: 'code',
           children: result.unifiedAST,
@@ -379,7 +381,7 @@ export class DiffHunksRenderer<LAnnotation = undefined> {
       );
     }
     if (result.deletionsAST != null) {
-      result.preNode.children.push(
+      tempChildren.push(
         createHastElement({
           tagName: 'code',
           children: result.deletionsAST,
@@ -391,7 +393,7 @@ export class DiffHunksRenderer<LAnnotation = undefined> {
       );
     }
     if (result.additionsAST != null) {
-      result.preNode.children.push(
+      tempChildren.push(
         createHastElement({
           tagName: 'code',
           children: result.additionsAST,
@@ -402,7 +404,10 @@ export class DiffHunksRenderer<LAnnotation = undefined> {
         })
       );
     }
-    return toHtml(result.preNode);
+    result.preNode.children = tempChildren;
+    const html = toHtml(result.preNode);
+    result.preNode.children = _children;
+    return html;
   }
 
   renderPartialHTML(
@@ -513,7 +518,7 @@ export class DiffHunksRenderer<LAnnotation = undefined> {
           linesAST.push(createSeparator({ type: 'empty' }));
         }
       }
-      for (const line of this.getNodesToRender(nodes)) {
+      for (const line of this.getLineNodes(nodes)) {
         linesAST.push(line);
       }
       if (
@@ -1020,7 +1025,7 @@ export class DiffHunksRenderer<LAnnotation = undefined> {
     return { unifiedDecorations, deletionDecorations, additionDecorations };
   }
 
-  private getNodesToRender(nodes: Root): ElementContent[] {
+  private getLineNodes(nodes: Root): ElementContent[] {
     let firstChild: RootContent | Element | Root | null = nodes.children[0];
     while (firstChild != null) {
       if (firstChild.type === 'element' && firstChild.tagName === 'code') {
