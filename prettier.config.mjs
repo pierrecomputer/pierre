f36e8696881148381cefd694633ce3d312c21a3f
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-10-22 22:56:19 -0700

    Rename prettier config to not be a hidden file
    Hidden files suck

---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  importOrder: ['<THIRD_PARTY_MODULES>', '^\\.'],
+  importOrderSortSpecifiers: true,
+  importOrderSeparation: true,
+  plugins: ['@trivago/prettier-plugin-sort-imports'],
+};
