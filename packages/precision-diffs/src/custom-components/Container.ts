7883e4b7d45cd70318f4b42081f96afb5be3c758
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-10-23 16:52:24 -0700

    Fix custom container to match ssr container

--- packages/precision-diffs/src/custom-components/Container.ts
+++ packages/precision-diffs/src/custom-components/Container.ts
@@ -2,21 +2,27 @@ import styles from '../style.css?raw';
 
 // If HTMLElement is undefined it usually means we are in a server environment
 // so best to just not do anything
-if (typeof HTMLElement !== 'undefined') {
+if (
+  typeof HTMLElement !== 'undefined' &&
+  customElements.get('file-diff') == null
+) {
   let sheet: CSSStyleSheet | undefined;
 
   class PJSContainer extends HTMLElement {
     constructor() {
       super();
-      this.attachShadow({ mode: 'open' });
+      // If shadow root is already open, we can sorta assume the CSS is already
+      // in place
+      if (this.shadowRoot != null) {
+        return;
+      }
+      const shadowRoot = this.attachShadow({ mode: 'open' });
       if (sheet == null) {
         sheet = new CSSStyleSheet();
         sheet.replaceSync(styles);
       }
       // TODO(amadeus): Figure out how to adapt user generated styles into here?
-      if (this.shadowRoot?.adoptedStyleSheets != null) {
-        this.shadowRoot.adoptedStyleSheets = [sheet];
-      }
+      shadowRoot.adoptedStyleSheets = [sheet];
     }
     // Not sure if we need to do anything here yet...
     // connectedCallback() {
@@ -24,5 +30,5 @@ if (typeof HTMLElement !== 'undefined') {
     // }
   }
 
-  customElements.define('pjs-container', PJSContainer);
+  customElements.define('file-diff', PJSContainer);
 }
