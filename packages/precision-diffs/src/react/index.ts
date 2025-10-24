8bbec1c2e1582cd51a85254baa80117b53aada1b
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-10-24 15:50:52 -0700

    Hopefully fix typescript

--- packages/precision-diffs/src/react/index.ts
+++ packages/precision-diffs/src/react/index.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="./jsx.d.ts" />
+
 export * from './FileDiff';
 export * from './File';
 export * from './utils/useStableCallback';
