c21ea28132985772371cb2b9b5ea934ac81e86ea
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-10-24 18:07:05 -0700

    Sorting exports, bcuz

--- packages/precision-diffs/src/react/index.ts
+++ packages/precision-diffs/src/react/index.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./jsx.d.ts" />
 
-export * from './FileDiff';
 export * from './File';
+export * from './FileDiff';
 export * from './utils/useStableCallback';
