a8866613f7faf9d0f234055a9f3c4f11faa04a8b
Amadeus Demarzi <amadeusdemarzi@gmail.com>
2025-12-03 23:23:05 -0800

    Make 2 builds of the worker script, until i can get alex to tube it

--- packages/precision-diffs/tsdown.config.ts
+++ packages/precision-diffs/tsdown.config.ts
@@ -4,7 +4,12 @@ import { type UserConfig, defineConfig } from 'tsdown';
 
 const config: UserConfig = defineConfig([
   {
-    entry: ['src/**/*.ts', 'src/**/*.tsx', '!src/worker/worker.ts'],
+    entry: [
+      'src/**/*.ts',
+      'src/**/*.tsx',
+      '!src/worker/worker.ts',
+      '!src/worker/worker-portable.ts',
+    ],
     loader: {
       '.css': 'text',
     },
@@ -40,11 +45,17 @@ const config: UserConfig = defineConfig([
     outDir: 'dist/worker',
     tsconfig: './tsconfig.json',
     clean: false,
+    dts: { sourcemap: true },
+    platform: 'neutral',
+  },
+  {
+    entry: ['src/worker/worker-portable.ts'],
+    outDir: 'dist/worker',
+    tsconfig: './tsconfig.json',
+    clean: false,
     unbundle: false,
     noExternal: [/.*/],
-    dts: {
-      sourcemap: true,
-    },
+    dts: { sourcemap: true },
     platform: 'neutral',
     format: 'esm',
   },
