a0af75b12632eac6716e6f1946880f1bf65935fc
Alex Sexton <alex@pierre.co>
2026-01-07 15:29:50 -0600

    get a very loose hydration pattern working so we can create the component instance in both the ssr and client versions

--- apps/docs/app/file-tree/ClientPage.tsx
+++ apps/docs/app/file-tree/ClientPage.tsx
@@ -1,19 +1,54 @@
 'use client';
 
-import { FileTree } from '@pierre/file-tree';
+import { FileTree, type FileTreeOptions } from '@pierre/file-tree';
+import { FileTree as FileTreeReact } from '@pierre/file-tree/react';
 import { useEffect, useRef } from 'react';
 
-export function ClientPage() {
+const sharedFileTreeOptions: FileTreeOptions<any> = {
+  config: {
+    rootItemId: 'root-item',
+    getItemName: (item: any) => item.itemName,
+    isItemFolder: (_item) => true,
+    dataLoader: {
+      getItem: (itemId) => {
+        return {
+          id: itemId,
+          itemName: `item ${itemId}`,
+          isFolder: false,
+          childrenIds: [],
+        };
+      },
+      getChildren: (_itemId) => [],
+    },
+  },
+};
+
+export function ClientPage({
+  preloadedFileTreeHtml,
+}: {
+  preloadedFileTreeHtml: string;
+}) {
   const isRendered = useRef(false);
   useEffect(() => {
     if (isRendered.current) return;
     isRendered.current = true;
-    const fileTree = new FileTree();
+    const fileTree = new FileTree(sharedFileTreeOptions);
     fileTree.render({
       containerWrapper:
         document.getElementById('test-file-tree-elem') ?? undefined,
     });
   }, []);
 
-  return null;
+  return (
+    <>
+      <div className="w-2/3">
+        <h2>React SSR File Tree</h2>
+        <FileTreeReact
+          options={sharedFileTreeOptions}
+          className="border border-gray-300"
+          prerenderedHTML={preloadedFileTreeHtml}
+        />
+      </div>
+    </>
+  );
 }
