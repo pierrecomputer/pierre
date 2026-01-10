7904ebcc8df692ec0b80eab3eb97e1c45611edc2
Alex Sexton <alex@pierre.co>
2026-01-09 18:22:12 -0600

    get server rendering from the same html generator as the dom

--- apps/docs/app/file-tree/page.tsx
+++ apps/docs/app/file-tree/page.tsx
@@ -1,22 +1,5 @@
 import { ClientPage } from './ClientPage';
 
-const preloadedFileTreeHtml = `<style>
-@layer base, theme, unsafe;
-
-@layer base {
-  :host {
-    color-scheme: light dark;
-    display: block;
-    font-family:
-      'SF Mono', Monaco, Consolas, 'Ubuntu Mono', 'Liberation Mono',
-      'Courier New', monospace;
-  }
-}
-</style>
-<div id="file-tree-div-wrapper">
-  <div>File Tree Fake</div>
-</div>`;
-
 export default function Home() {
   return (
     <div className="grid grid-cols-2 gap-4 p-4">
@@ -25,7 +8,7 @@ export default function Home() {
         <div id="test-file-tree-elem" className="border border-gray-300" />
       </div>
 
-      <ClientPage preloadedFileTreeHtml={preloadedFileTreeHtml} />
+      <ClientPage />
     </div>
   );
 }
