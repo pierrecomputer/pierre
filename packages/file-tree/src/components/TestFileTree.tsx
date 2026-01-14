eaff9de66c5c2d48c9f91b113647577591262193
Alex Sexton <alex@pierre.co>
2026-01-14 15:03:04 -0600

    begin ssr setup

--- packages/file-tree/src/components/TestFileTree.tsx
+++ packages/file-tree/src/components/TestFileTree.tsx
@@ -1,5 +1,16 @@
 import type { JSX } from 'preact';
 
 export function TestFileTree(): JSX.Element {
-  return <div>TestFileTree</div>;
+  return (
+    <div>
+      TestFileTree
+      <button
+        onClick={() => {
+          console.log('clicked');
+        }}
+      >
+        Click me
+      </button>
+    </div>
+  );
 }
