326f35b3d2b85121386da804a59df39705854535
Alex Sexton <alex@pierre.co>
2026-03-19 16:03:42 -0500

    pretty solid state

--- apps/docs/app/truncate-dev/page.tsx
+++ apps/docs/app/truncate-dev/page.tsx
@@ -7,14 +7,26 @@ export default function TruncateDevPage() {
   }
 
   return (
-    <div>
+    <div
+      style={{
+        margin: '30px 20%',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '10px',
+        backgroundColor: 'white',
+      }}
+    >
+      <OverflowText backgroundColor="white">
+        This is a test of the overflow text component.
+      </OverflowText>
+
+      <OverflowText backgroundColor="white" renderOverflowMarker="………">
+        This is a test of the overflow text component.
+      </OverflowText>
+
       <OverflowText
-        baseCss={`
-          color: red;
-        `}
-        onOverflow={`
-          color: blue;
-        `}
+        backgroundColor="white"
+        renderOverflowMarker={() => <span style={{ color: 'red' }}>…</span>}
       >
         This is a test of the overflow text component.
       </OverflowText>
--- packages/truncate/src/react/components/OverflowText.tsx
+++ packages/truncate/src/react/components/OverflowText.tsx
@@ -1,31 +1,103 @@
-import { useId, useMemo } from 'react';
+import { type ReactNode, useId, useMemo } from 'react';
 import type { PropsWithChildren } from 'react';
 
+export interface RenderOverflowMarkerProps extends PropsWithChildren {}
+
 export interface OverflowTextProps extends PropsWithChildren {
-  baseCss?: string;
-  onOverflow?: string;
+  backgroundColor?: string;
+  renderOverflowMarker?:
+    | ReactNode
+    | ((props: RenderOverflowMarkerProps) => ReactNode);
 }
 
 export function OverflowText({
   children,
-  baseCss,
-  onOverflow,
+  backgroundColor,
+  renderOverflowMarker = '…',
   ...props
 }: OverflowTextProps) {
   const id = useId();
 
   const styleBlock = useMemo(() => {
-    const baseStyles = baseCss ? `#${id} { ${baseCss} }` : '';
-    const onOverflowStyles = onOverflow
-      ? `@media (max-width: 768px) { #${id} { ${onOverflow} } }`
-      : '';
-    return `${baseStyles}${onOverflowStyles}`;
-  }, [baseCss, id, onOverflow]);
+    return `[data-truncate-overflow-marker-inner] { opacity: 0; }
+@container measure (height >= 1.1lh) { [data-truncate-overflow-marker-inner] { opacity: 1; } }`;
+  }, []);
 
   return (
-    <div id={id} {...props}>
+    <div
+      id={id}
+      data-truncate-container
+      {...props}
+      style={{ height: '1lh', overflow: 'hidden' }}
+    >
       <style>{styleBlock}</style>
-      {children}
+
+      <div
+        data-truncate-grid-container
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '0 minmax(0, max-content) auto',
+          position: 'relative',
+        }}
+      >
+        <div
+          data-truncate-overflow-marker
+          style={{
+            container: 'measure / size',
+            overflow: 'visible',
+            userSelect: 'none',
+            pointerEvents: 'none',
+          }}
+        >
+          <div
+            data-truncate-overflow-marker-inner
+            style={{
+              display: 'inline-flex',
+              position: 'absolute',
+              // css mask to hide the text underneath
+              // mask: 'linear-gradient(to right, transparent, black 100%)',
+              zIndex: 2,
+              backgroundColor,
+            }}
+          >
+            {typeof renderOverflowMarker === 'function'
+              ? renderOverflowMarker({ children })
+              : renderOverflowMarker}
+          </div>
+        </div>
+        <div
+          style={{
+            overflow: 'hidden',
+            zIndex: 1,
+            textAlign: 'right',
+          }}
+        >
+          <div
+            data-truncate-visible-content
+            style={{
+              overflow: 'hidden',
+              whiteSpace: 'nowrap',
+              direction: 'rtl',
+            }}
+          >
+            <span style={{ unicodeBidi: 'plaintext' }}>{children}</span>
+          </div>
+          <div
+            data-truncate-overflow-content
+            style={{
+              opacity: 0,
+              pointerEvents: 'none',
+              userSelect: 'none',
+              wordBreak: 'break-all',
+              marginTop: '-1lh',
+              direction: 'rtl',
+            }}
+          >
+            <span style={{ unicodeBidi: 'plaintext' }}>{children}</span>
+          </div>
+        </div>
+        <div data-truncate-fill></div>
+      </div>
     </div>
   );
 }
