ec657543407664c34c7e883e0dc844a364c99dca
Je Xia <i@jex.me>
2026-07-14 18:49:26 +0800

    fix merge

--- packages/diffs/src/editor/index.ts
+++ packages/diffs/src/editor/index.ts
@@ -1,5 +1,4 @@
+export type { Marker, MarkerSeverity } from './marker';
+export type { SelectionActionContext } from './selectionAction';
 export * from './editor';
 export * from './textDocument';
-export type { Marker, MarkerSeverity } from './marker';
-export type { AutoSurround } from './selection';
-export type { SelectionActionContext } from './selectionAction';
